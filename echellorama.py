da5662838fb5d10a9a863d419adf2c6e384fc03e
Iguanacolossus <Bushinsky@hotmail.com>
2013-08-02 11:15:10 -0600

    now using find_peaks_cwt()

--- echellorama.py
+++ echellorama.py
@@ -14,6 +14,7 @@ import astropy.io.fits as fits
 #import aplpy
 
 import numpy as np
+from scipy import signal
 
 
 def main():
@@ -70,7 +71,7 @@ def main():
     plt.plot(y,xrev)
     plt.ylabel('pixels(y)')
     plt.xlabel('Intensity')
-    plt.vlines(bgcutoff,[0],1000,label='background cutoff')
+    #plt.vlines(bgcutoff,[0],1000,label='background cutoff')
     plt.hlines(chunk,[-1000],[5000],color='r')
     plt.vlines([-1000,5000],minv,maxv,color='r')
     plt.show()
@@ -82,49 +83,59 @@ def main():
     ychunk = y[index1:index2]
     #reversing x for the sake of the plot
     xrevchunk = xchunk[::-1]
-
-    #creating a new baseline
-    for i in range(0,xchunk.size):
-        if ychunk[i] < bgcutoff:
-            ychunk[i] = 0
-
-
-    #plt.plot(ychunk,xrevchunk)
-    #plt.show()
-
-    bounds = []
-    a = ychunk
-    # find the bounds of a order by indice in the array
-    for i in range(1, len(a)):
-        if a[i-1] == 0 and a[i] > 0:
-           # print(i , "begin hump index")
-            bounds.append(i)
-        if a[i-1] > 0 and a[i] == 0:
-           # print(i-1, "end hump index")
-            bounds.append(i - 1)
-
-    # now 'bounds' is a list of the indices of the array were a #order begins and ends
-  
-    # 'bounds' but now grouped
-    grpbounds = list(group(bounds, 2))
-
-    centers = []
-    # finding max with in the bouds of the orders and reading them #in
-    # to 'centers' list
-    for i in range(len(grpbounds)):
-        centers.append(max(a[grpbounds[i][0]:grpbounds[i][1]+1]))
-   
- 
-    #print a.index(centers)
+    #plt.subplot(123)
+####  this is my manual code for making a zero backgrouns then finding the value at each peak############################
+#    #creating a new baseline
+#   for i in range(0,xchunk.size):
+#        if ychunk[i] < bgcutoff:
+#            ychunk[i] = 0
+#
+#
+#    #plt.plot(ychunk,xrevchunk)
+#    #plt.show()
+#
+#    bounds = []
+#    a = ychunk
+#    # find the bounds of a order by indice in the array
+#    for i in range(1, len(a)):
+#        if a[i-1] == 0 and a[i] > 0:
+#           # print(i , "begin hump index")
+#            bounds.append(i)
+#        if a[i-1] > 0 and a[i] == 0:
+#           # print(i-1, "end hump index")
+#            bounds.append(i - 1)
+#
+#    # now 'bounds' is a list of the indices of the array were a #order begins and ends
+#  
+#    # 'bounds' but now grouped
+#    grpbounds = list(group(bounds, 2))
+#
+#    centers = []
+#    # finding max with in the bouds of the orders and reading them #in
+#    # to 'centers' list
+#    for i in range(len(grpbounds)):
+#        centers.append(max(a[grpbounds[i][0]:grpbounds[i][1]+1]))
+#   
+# 
+#    #print a.index(centers)
     
 # defining a function to group the indecies of 'bounds' to # sets of
 # (begin order, end order)
-def group(lst,n):
-    for i in range(0, len(lst), n):
-       val=lst[i:i+n]
-       if len(val) == n:
-          yield tuple(val)
-
+#def group(lst,n):
+#    for i in range(0, len(lst), n):
+#       val=lst[i:i+n]
+#       if len(val) == n:
+#          yield tuple(val)
+############################################################################################################################3
+    
+    #using scipy.signal.find_peaks_cwt() to find centers of orders.  this required scipy version .11.0 or greater
+    peakind=signal.find_peaks_cwt(ychunk,np.arange(3,15))
+   
+    plt.plot(xchunk,ychunk)
+    plt.vlines(xchunk[peakind],-1000,5000,color='purple')
+    plt.title('chunk of data with centers found by find_peaks_cwt()')
+    
+    plt.show()
 
 
 if __name__ == '__main__':
